9bcb5239698e13f3b9d8787c1a2bde9466edaaef
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-01-20 14:34:24 +0100

    feat(frontend): add folder structure and basic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-function App() {
-  return <h1>Init</h1>;
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={< Users />} />
+        <Route path='/places/new' element={<NewPlace />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Router >
+  );
 }
-
-export default App;
