f8ed724fec8e642b676eebb260b38a4e20fc01a1
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-01-24 12:43:20 +0100

    feat(frontend): add style to buttons in PlaceItem

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 
 import './PlaceItem.css';
 
@@ -17,9 +18,9 @@ export default function PlaceItem(props) {
           <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-          <button>VIiew on map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <Button inverse>VIiew on map</Button>
+          <Button to={`/places/${props.id}`}>Edit</Button>
+          <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
