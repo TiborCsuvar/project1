0b427a7914494db3727c520a6012e2ae077ccde2
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-01-25 13:53:39 +0100

    feat(frontend): add validation to Add Place form

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validator";
 
 import "./NewPlace.css";
 
 export default function NewPlace() {
   return (
     <form className="place-form">
-      <label></label>
-      <Input type="text" label="Title" element="input" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title" />
     </form>
   );
 }
