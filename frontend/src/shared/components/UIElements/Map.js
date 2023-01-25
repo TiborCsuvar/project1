9f62375f9c313e59662116263d56cefe7cff2af6
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-01-25 10:22:11 +0100

    feat(frontend): add Google Maps to Map component

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -8,18 +8,12 @@ export default function Map(props) {
   const { center, zoom } = props;
 
   useEffect(() => {
-    new window.ol.Map({
-      target: mapRef.current.id,
-      layers: [
-        new window.ol.layer.Tile({
-          source: new window.ol.source.OSM()
-        })
-      ],
-      view: new window.ol.View({
-        center: window.ol.proj.fromLonLat([center.lng, center.lat]),
-        zoom: zoom
-      })
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom
     });
+
+    new window.google.maps.Marker({ position: center, map: map });
   }, [center, zoom]);
 
   return (
