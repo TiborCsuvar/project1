d4826ff2173e9586621095336d794777c015f9cd
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-01-30 09:19:41 +0100

    feat(frontend): add /auth route and the Auth component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Users from './user/pages/Users';
 import UserPlaces from './places/pages/UserPlaces';
 import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
           <Route path='/:userId/places' exact="true" element={<UserPlaces />} />
           <Route path='/places/new' exact="true" element={<NewPlace />} />
           <Route path='/places/:placeId' exact="true" element={<UpdatePlace />} />
+          <Route path="auth" exact="true" element={<Auth />} />
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </main>
