9aa573a95412c57b9504b6600c9b1f4eceeaf43b
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-02-09 14:23:00 +0100

    feat(backend): connect the app to MongoDB Cloud

--- backend/config.ts
+++ backend/config.ts
@@ -5,4 +5,6 @@ config();
 export default {
   port: process.env.SERVER_PORT,
   api_key: process.env.API_KEY,
+  database_username: process.env.DATABASE_USERNAME,
+  database_password: process.env.DATABASE_PASSWORD,
 };
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import config from "../config";
+import mongoose from "mongoose";
 
 import placesRoutes from "./routes/places-routes";
 import usersRoutes from "./routes/users-routes";
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 const PORT = config.port || 8081;
+const DATABASE_USERNAME = config.database_username;
+const DATABASE_PASSWORD = config.database_password;
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
@@ -26,8 +29,17 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured." });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at port: ${PORT}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@cluster0.hta28o9.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening at port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 export default app;
