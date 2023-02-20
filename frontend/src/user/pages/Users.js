33d08ccc6386439c729fc4fecda7be633b32ab76
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-02-20 16:02:14 +0100

    feat(frontend): add GET request to http://localhost:3005/api/users endpoint

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 export default function Users() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLadedUsers] = useState();
 
-  const USERS = [
-    {
-      id: 1,
-      name: 'Joe Kort',
-      image: '/cat.jpg',
-      places: 4
-    }
-  ];
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:3005/api/users")
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(response.message);
+        }
+
+        setLadedUsers(responseData);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false)
+
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  }
 
   return (
-    <UsersList usersDummyArray={USERS} />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading &&
+        <div className="center">
+          <LoadingSpinner />
+        </div>}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
   );
 }
