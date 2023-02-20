e9c5534110076e8be21e4fa67ef7b4d5ddda6fbf
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-02-20 13:42:09 +0100

    feat(frontend): add POST request to http://localhost:3005/api/users/login endpoint

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -59,11 +59,37 @@ export default function Auth() {
   const authSubmitHandler = async event => {
     event.preventDefault();
 
+    setIsLoading(true);
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:3005/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          }
+          ,
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        }
+        );
+        const responseData = await response.json();
 
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        auth.login();
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setError(error.message || "Something went wrong, please try again.");
+      }
     } else {
       try {
-        setIsLoading(true);
+
         const response = await fetch("http://localhost:3005/api/users/signup", {
           method: "POST",
           headers: {
@@ -83,7 +109,6 @@ export default function Auth() {
           throw new Error(responseData.message);
         }
 
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (error) {
