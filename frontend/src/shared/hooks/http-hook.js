b2e47c6aee86858bacddd930887a2d7d82db76eb
Csuvár Tibor <tibor.csuvar@gmail.com>
2023-02-21 13:02:56 +0100

    feat(frontend): rewrite  GET request http://localhost:3005/api/users

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -1,51 +1,52 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback, /*useRef, useEffect */ } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const activeHttpRequest = useRef([]);
+  // const activeHttpRequest = useRef([]);
 
-  const sendRequest = useCallback(async (url, method = "GET", headers = {}, body = null) => {
-    setIsLoading(true);
-    const httpAbortController = new AbortController();
-    activeHttpRequest.current.push(httpAbortController);
+  const sendRequest = useCallback(
+    async (url, method = "GET", headers = {}, body = null) => {
+      setIsLoading(true);
+      //  const httpAbortController = new AbortController();
+      //  activeHttpRequest.current.push(httpAbortController);
 
-    try {
-      const response = await fetch(url, {
-        method: method,
-        headers: headers,
-        body: body,
-        signal: httpAbortController.signal
-      });
-      const responseData = await response.json();
+      try {
+        const response = await fetch(url, {
+          method: method,
+          headers: headers,
+          body: body,
+          //  signal: httpAbortController.signal
+        });
+        const responseData = await response.json();
 
-      activeHttpRequest.current = activeHttpRequest.current.filter(reqCtrl => reqCtrl !== httpAbortController);
+        //  activeHttpRequest.current = activeHttpRequest.current.filter(reqCtrl => reqCtrl !== httpAbortController);
 
-      if (!response.ok) {
-        throw new Error(responseData.message);
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        return responseData;
+
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
-      return responseData;
 
-    } catch (error) {
-      setError(error.message);
-      setIsLoading(false);
-      throw error;
-    }
-
-  }, []);
+    }, []);
 
   const clearError = () => {
     setError(null);
   };
 
-  useEffect(() => {
-    return () => {
-      activeHttpRequest.current.forEach(abortCtrl => abortCtrl.abort());
-    };
-  }, []);
-
+  /*  useEffect(() => {
+      return () => {
+        activeHttpRequest.current.forEach(abortCtrl => abortCtrl.abort());
+      };
+    }, []);
+  */
   return {
     isLoading: isLoading,
     error: error,
